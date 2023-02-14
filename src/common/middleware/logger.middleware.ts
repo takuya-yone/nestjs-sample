92ac31176080e52795bbfd4c5bed01d853b5f09e
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-02-15 04:46:55 +0900

    up

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('LoggerMiddleware');
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    // console.log('Request...');
+    logger.log(req.url);
+    next();
+  }
+}
