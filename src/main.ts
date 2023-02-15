fb90b63bf48e86e570cef8c2c91c742416ccbdb6
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-02-15 13:13:05 +0900

    up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log'],
   });
+  const config = new DocumentBuilder()
+    .setTitle('nestjs example')
+    .setDescription('nestjs-sample API description')
+    .setVersion('1.0')
+    .addTag('nestjs-sample')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
